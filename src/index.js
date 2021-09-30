8ed40ff7d7c7c223274bc46af8ed0d2817e99c18
RijlanM10 <rijlan01@gmail.com>
2021-09-30 21:45:31 +0700

    fix: responsive

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const searchButtonRef = document.querySelector('.search-button');
 const searchFormRef = document.querySelector('.search-form');
+const searchInputRef = document.querySelector('.search-input')
 
 searchButtonRef.addEventListener('click', () => {
     searchFormRef.classList.toggle('open');
-})
+    searchInputRef.focus();
+});
